9b0f578d9e6dc69c75736295a0fab08d2a778135
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-25 17:49:17 +0800

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -95,6 +95,10 @@ class TitleState extends MusicBeatState
 		Paths.clearStoredMemory();
 		Paths.clearUnusedMemory();
 
+                #if android
+                FlxG.android.preventDefaultKeys = [BACK];
+                #end
+
 		#if LUA_ALLOWED
 		Paths.pushGlobalMods();
 		#end
