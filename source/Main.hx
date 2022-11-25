990c1c66c847ba783b2c37d8451483be0563eedf
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-11-25 17:58:45 +0800

    Ok...

--- source/Main.hx
+++ source/Main.hx
@@ -89,7 +89,6 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -97,7 +96,6 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
 
 		#if html5
 		FlxG.autoPause = false;
