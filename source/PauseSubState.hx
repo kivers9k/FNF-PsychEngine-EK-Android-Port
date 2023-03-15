9bee4a6d5c75e9548ebb35ef585583faa8ea770a
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-15 18:12:27 +0800

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -136,7 +136,7 @@ class PauseSubState extends MusicBeatSubstate
 
                 #if android
                 addVirtualPad(UP_DOWN, A);
-                addVirtualPadCamera();
+                virtualpad.cameras = [PlayState.instance.camHUD];
                 #end
 	}
 
