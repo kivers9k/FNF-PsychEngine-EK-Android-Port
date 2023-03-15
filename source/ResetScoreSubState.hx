4730099052b804d08092dc1fd5e879aa83c8c19a
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-15 21:51:46 +0800

    Update ResetScoreSubState.hx

--- source/ResetScoreSubState.hx
+++ source/ResetScoreSubState.hx
@@ -69,6 +69,10 @@ class ResetScoreSubState extends MusicBeatSubstate
 		noText.x += 200;
 		add(noText);
 		updateOptions();
+		#if android
+		addVirtualPad(LEFT_RIGHT, A_B);
+		virtualpad.cameras = [PlayState.instance.camHUD];
+		#end
 	}
 
 	override function update(elapsed:Float)
@@ -89,7 +93,12 @@ class ResetScoreSubState extends MusicBeatSubstate
 		}
 		if(controls.BACK) {
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
+			#if android
+			flixel.addons.transition.FlxTransitionableState.skipNextTransOut = true;
+			FlxG.resetState();
+			#else
 			close();
+			#end
 		} else if(controls.ACCEPT) {
 			if(onYes) {
 				if(week == -1) {
@@ -99,7 +108,12 @@ class ResetScoreSubState extends MusicBeatSubstate
 				}
 			}
 			FlxG.sound.play(Paths.sound('cancelMenu'), 1);
+			#if android
+			flixel.addons.transition.FlxTransitionableState.skipNextTransOut = true;
+			FlxG.resetState();
+			#else
 			close();
+			#end
 		}
 		super.update(elapsed);
 	}
