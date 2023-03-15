0383f5ba075bdadb48c1d362b89fd71a79ac9495
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-15 19:06:59 +0800

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -9,6 +9,7 @@ import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
+import android.flixel.FlxVirtualPad;
 
 class GameOverSubstate extends MusicBeatSubstate
 {
