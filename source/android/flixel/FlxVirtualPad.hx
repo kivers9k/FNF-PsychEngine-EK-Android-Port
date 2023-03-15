113b2a2f220ce79f1ba45af0d3a8be2b7164a6e3
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-15 08:46:56 +0800

    Also this

--- source/android/flixel/FlxVirtualPad.hx
+++ source/android/flixel/FlxVirtualPad.hx
@@ -160,6 +160,7 @@ class FlxVirtualPad extends FlxSpriteGroup
 		button.immovable = true;
 		button.scrollFactor.set();
 		button.color = Color;
+                button.alpha = 0.5;
 		#if FLX_DEBUG
 		button.ignoreDrawDebug = true;
 		#end
