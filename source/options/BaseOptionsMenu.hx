6381b8f0e7ce382e3e23d866408e6a9b00b1b250
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-15 19:30:45 +0800

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -127,7 +127,7 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		
 		#if android
 		addVirtualPad(LEFT_FULL, A_B_C);
-		virtualpad.cameras = [PlayState.instance.camHUD];
+		_virtualpad.cameras = [PlayState.instance.camHUD];
 		#end
 	}
 
