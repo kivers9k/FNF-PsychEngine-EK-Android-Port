15e12f77b7b8ed0e98df62afac4b8702adbafb8d
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-16 18:59:59 +0800

    chart editor can now be accessed on pause
    for people who dont have or wanna use gamepad

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -21,7 +21,7 @@ class PauseSubState extends MusicBeatSubstate
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
 	var menuItems:Array<String> = [];
-	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty', 'Exit to menu'];
+	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty', 'Chart Editor', 'Exit to menu'];
 	var difficultyChoices = [];
 	var curSelected:Int = 0;
 
@@ -136,7 +136,14 @@ class PauseSubState extends MusicBeatSubstate
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 
                 #if android
-                addVirtualPad(UP_DOWN, A);
+		if (PlayState.chartingMode)
+                {
+                        addVirtualPad(FULL, A);
+                }
+                else
+                {
+                        addVirtualPad(UP_DOWN, A);
+                }
                 virtualPad.cameras = [PlayState.instance.camHUD];
                 #end
 	}
@@ -257,6 +264,9 @@ class PauseSubState extends MusicBeatSubstate
 					PlayState.instance.botplayTxt.visible = PlayState.instance.cpuControlled;
 					PlayState.instance.botplayTxt.alpha = 1;
 					PlayState.instance.botplaySine = 0;
+				case "Chart Editor":
+		                        PlayState.chartingMode = true;
+                                        MusicBeatState.switchState(new editors.ChartingState());
 				case "Exit to menu":
 					PlayState.deathCounter = 0;
 					PlayState.seenCutscene = false;
