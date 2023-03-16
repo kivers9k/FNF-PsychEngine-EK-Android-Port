b4f37e31ad8e6ea4fcf94434feadc2bf6639d5b4
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-16 19:21:23 +0800

    Update CreditsState.hx
    almost forgor 💀

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -167,6 +167,10 @@ class CreditsState extends MusicBeatState
 		bg.color = getCurrentBGColor();
 		intendedColor = bg.color;
 		changeSelection();
+		
+		#if android
+  	        addVirtualPad(UP_DOWN, A_B);
+                #end
 		super.create();
 	}
 
