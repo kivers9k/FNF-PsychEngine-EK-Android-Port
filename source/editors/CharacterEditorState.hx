fcb4de325ec938aa35eb25ce75781f2078e5cde6
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-03-30 07:10:30 +0800

    Update CharacterEditorState.hx

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -203,7 +203,7 @@ class CharacterEditorState extends MusicBeatState
 		
 		#if android
 		addVirtualPad(LEFT_FULL, A_B_C_D_V_X_Y_Z);
-		addVirtualPadCamera();
+		virtualPad.cameras = [camHUD];
 		#end
 
 		super.create();
