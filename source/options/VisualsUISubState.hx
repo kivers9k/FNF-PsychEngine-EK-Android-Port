e5f9fe92d3a36b6dd26539be92e9c544bba2c77f
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-06-14 15:15:50 +0800

    Add files via upload

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -89,7 +89,7 @@ class VisualsUISubState extends BaseOptionsMenu
 		option.decimals = 1;
 		addOption(option);
 		
-		#if !mobile
+		#if android
 		var option:Option = new Option('FPS Counter',
 			'If unchecked, hides FPS Counter.',
 			'showFPS',
