0aee997d2bfcea48179e3de1de6ed119ca6f7734
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-06-20 11:21:17 +0800

    Add files via upload

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -200,7 +200,7 @@ class NoteOffsetState extends MusicBeatState
 
 		#if android
         addVirtualPad(LEFT_RIGHT, A_B_C);
-        addVirtualPadCamera();
+        _virtualpad.cameras = [camHUD];
         #end
 
 		super.create();
