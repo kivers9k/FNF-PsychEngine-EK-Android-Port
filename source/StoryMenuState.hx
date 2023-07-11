34a0925ab0a68a19a4950f27b878a421e04622d8
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-07-11 22:35:34 +0800

    ABXY button
    X and Y button is like freeplay

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -186,7 +186,7 @@ class StoryMenuState extends MusicBeatState
 		changeDifficulty();
 
         #if android
-        addVirtualPad(FULL, A_B_C);
+        addVirtualPad(FULL, A_B_X_Y);
         #end
 
 		super.create();
@@ -248,13 +248,19 @@ class StoryMenuState extends MusicBeatState
 			else if (upP || downP)
 				changeDifficulty();
 
-			if(FlxG.keys.justPressed.CONTROL)
+		    if(FlxG.keys.justPressed.CONTROL #if android || _virtualpad.buttonX.justPressed #end)
 			{
+				#if android
+				removeVirtualPad();
+				#end
 				persistentUpdate = false;
 				openSubState(new GameplayChangersSubstate());
 			}
-			else if(controls.RESET)
+			else if(controls.RESET #if android || _virtualpad.buttonY.justPressed #end)
 			{
+				#if android
+				removeVirtualPad();
+				#end
 				persistentUpdate = false;
 				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
 				//FlxG.sound.play(Paths.sound('scrollMenu'));
