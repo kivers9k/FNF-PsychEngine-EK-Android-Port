d31d87d46e33054323f3cb816fdd371f316b04be
Kivers69 <123798197+Kivers69@users.noreply.github.com>
2023-07-12 15:59:34 +0800

    Re add the og pe ek flashing cuz yes

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -1,5 +1,6 @@
 package;
 
+
 import flixel.tweens.FlxEase;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -11,42 +12,96 @@ import lime.app.Application;
 import flixel.addons.transition.FlxTransitionableState;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
+#if lime
+import lime.system.System;
+#end
+
+using StringTools;
 
 class FlashingState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
 
 	var warnText:FlxText;
+	var saveDataPath:String = '';
+	var displaySaveDataPath:String = '';
+	
+	var bg:FlxSprite;
+	var warnTextY:Float = 0;
+	var manual:FlxSprite;
+	var textNoAdvanced:String = "Hey, watch out!\n
+								This Mod contains some flashing lights!\n
+								Press A to disable them now or go to Options Menu.\nPress B to ignore this message.\n
+								You've been warned!";
+	var textAdvanced:String = "";
+
 	override function create()
 	{
 		super.create();
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		FlxG.save.data.firstTime = true;
+		FlxG.save.flush();
+
+		bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
-		#if android
-		warnText = new FlxText(0, 0, FlxG.width, "Hey, watch out!\n
-		This Mod contains some flashing lights!\n
-		Press A to disable them now or go to Options Menu.\n
-		Press B to ignore this message.\n
-		You've been warned!",
-		32);
+		FlxG.mouse.visible = true;
+		#if lime
+	 	saveDataPath = System.applicationStorageDirectory + 'tposejank\\';
+		displaySaveDataPath = StringTools.replace(saveDataPath, "\\", "/");
+
+		textAdvanced = 	
+		"Before use:\n\nEK uses a different save data folder than normal\nPsych Engine, so you are going to have to set your\noptions to what you're using.\n(Mobile users can ignore this message)\n" + 
+		#if lime
+		"Save data creation path:\n\n" + displaySaveDataPath + "\n" +
+		#if windows
+		"Press RESET to open this folder" +
+		#end
+		#end
+		"\n\n\n";
 		#end
 
-		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		manual = new FlxSprite(0, 0);
+		manual.frames = Paths.getSparrowAtlas('extra-keys/manual_book');
+		manual.animation.addByPrefix('normal', 'manual icon', 30, true);
+		manual.animation.addByPrefix('hover', 'manual icon hover', 30, true);
+		add(manual);
+		manual.x = FlxG.width - manual.width;
+		manual.y = FlxG.height - manual.height;
+		manual.animation.play('normal', true);
+
+		warnText = new FlxText(0, 0, FlxG.width,
+			textNoAdvanced,
+			8);
+		warnText.setFormat("VCR OSD Mono", 20, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
 
         #if mobile
-        addVirtualPad(NONE, A_B);
+        addVirtualPad(NONE, A_B_alt);
+        _virtualpad.x = -1025;
         #end
 	}
 
 	override function update(elapsed:Float)
 	{
 		if(!leftState) {
+			if (FlxG.mouse.overlaps(manual)) {
+				if (manual.animation.curAnim.name != 'hover') {
+					manual.animation.play('hover', true);
+				}
+				warnText.text = textAdvanced + textNoAdvanced;
+			} else {
+				if (manual.animation.curAnim != null && manual.animation.curAnim.name != 'normal') {
+					manual.animation.play('normal', true);
+				}
+				warnText.text = textNoAdvanced;
+			}
+			warnText.screenCenter(Y);
+
 			var back:Bool = controls.BACK;
 			if (controls.ACCEPT || back) {
+				FlxG.mouse.visible = false;
 				leftState = true;
 				FlxTransitionableState.skipNextTransIn = true;
 				FlxTransitionableState.skipNextTransOut = true;
@@ -68,6 +123,13 @@ class FlashingState extends MusicBeatState
 					});
 				}
 			}
+			#if (lime && windows)
+			if (controls.RESET) {
+				var command = "explorer " + saveDataPath.toLowerCase();
+				Sys.command(command);
+				trace("doing command: " + command);
+			}
+			#end
 		}
 		super.update(elapsed);
 	}
