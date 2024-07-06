e766b1e95761f221e3a84aaa1dbf8dea6382ff8a
kivers69 <georgechen1297@gmail.com>
2024-07-07 01:03:52 +0800

    Add files via upload

--- source/android/flixel/FlxHitbox.hx
+++ source/android/flixel/FlxHitbox.hx
@@ -5,45 +5,99 @@ import flixel.graphics.FlxGraphic;
 import flixel.group.FlxSpriteGroup;
 import flixel.ui.FlxButton;
 import flixel.FlxSprite;
+import flixel.FlxG;
 import flixel.util.FlxGradient;
 
 class FlxHitbox extends FlxSpriteGroup {
 	public var hitbox:FlxSpriteGroup;
 	public var array:Array<FlxButton> = [];
 
-	var colors:Array<Int> = {
-		red: 0xffFF0000,
-		green: 0xff00FF00,
-		cyan: 0xff00EDFF,
-		pink: 0xffE390E6,
-		white: 0xffFFFFFF,
-		yellow: 0xffFFFF00,
-		blue: 0xff0000FF,
-		purple: 0xffBB00FF,
-		darkRed: 0xffFF0000
-	};
+	/*red: 0xffFF0000
+	green: 0xff00FF00
+	cyan: 0xff00EDFF
+	pink: 0xffE390E6
+	white: 0xffFFFFFF
+	yellow: 0xffFFFF00
+	blue: 0xff0000FF
+	purple: 0xffBB00FF
+	darkRed: 0xffFF0000*/
+
 	var hitboxColor:Map<Int, Array<Int>> = [
-		1 => [colors.white],
-		2 => [colors.pink, colors.red],
-		3 => [colors.pink, colors.white, colors.red],
-		4 => [colors.pink, colors.cyan, colors.green , colors.red],
-		5 => [colors.pink, colors.cyan, colors.white, colors.green , colors.red],
-		6 => [colors.pink, colors.green, colors.red, colors.yellow, colors.cyan, colors.blue],
-		7 => [colors.pink, colors.green, colors.red, colors.white, colors.yellow, colors.cyan, colors.blue],
-		8 => [colors.pink, colors.cyan, colors.green , colors.red, colors.yellow, colors.purple, colors.red, colors.blue],
-		9 => [colors.pink, colors.cyan, colors.green , colors.red, colors.white, colors.yellow, colors.purple, colors.red, colors.blue],
+		1 => [
+			0xffFFFFFF
+		],
+		2 => [
+			0xffE390E6,
+			0xffFF0000
+		],
+		3 => [
+			0xffE390E6,
+			0xffFFFFFF,
+			0xffFF0000
+		],
+		4 => [
+			0xffE390E6,
+			0xff00EDFF,
+			0xff00FF00,
+			0xffFF0000
+		],
+		5 => [
+			0xffE390E6,
+			0xff00EDFF,
+			0xffFFFFFF,
+			0xff00FF00,
+			0xffFF0000
+		],
+		6 => [
+			0xffE390E6,
+			0xff00FF00,
+			0xffFF0000,
+			0xffFFFF00,
+			0xff00EDFF,
+			0xff0000FF
+		],
+		7 => [
+			0xffE390E6,
+			0xff00FF00,
+			0xffFF0000,
+			0xffFFFFFF,
+			0xffFFFF00,
+			0xff00EDFF,
+			0xff0000FF
+		],
+		8 => [
+			0xffE390E6,
+			0xff00EDFF,
+			0xff00FF00,
+			0xffFF0000,
+			0xffFFFF00,
+			0xffBB00FF,
+			0xffFF0000,
+			0xff0000FF
+		],
+		9 => [
+			0xffE390E6,
+			0xff00EDFF,
+			0xff00FF00,
+			0xffFF0000,
+			0xffFFFFFF,
+			0xffFFFF00,
+			0xffBB00FF,
+			0xffFF0000,
+			0xff0000FF
+		],
 		10 => []
 	];
-	public function new(mania:Int = 3) {
+	public function new(type:Int = 3) {
 		super();
 		hitbox = new FlxSpriteGroup();
 		hitbox.scrollFactor.set();
 		
-		var keyCount:Int = mania + 1;
+		var keyCount:Int = type + 1;
 		var hitboxWidth:Float = (FlxG.width / keyCount);
 		for (i in 0 ... keyCount) {
 			hitbox.add(createhitbox(hitboxWidth * i, 0, hitboxWidth, FlxG.height, hitboxColor[keyCount][i]));
-			array[i].push(hitbox.members[i]);
+			array[i] = hitbox.members[i];
 		}
 	}
 
